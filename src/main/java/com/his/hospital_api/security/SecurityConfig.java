02bd90c8890f10b8559b76fa7adf61e08daa3af4
Trương Công Minh <32140397+truongcongminh96@users.noreply.github.com>
2025-08-13 14:46:31 +0700

    Update responsive ui, add background image

--- src/main/java/com/his/hospital_api/security/SecurityConfig.java
+++ src/main/java/com/his/hospital_api/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/css/**", "/js/**").permitAll()
+                        .requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll()
                         .requestMatchers("/admin/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
                 )
